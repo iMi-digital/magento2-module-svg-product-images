cfe484e87d803c0036aeecbd2949fe9709137edc
Dominik Brachmanski <db@integer-net.de>
2020-05-14 14:20:21 +0200

    replace mime type read with magento method

--- SVGImageMagickAdapter.php
+++ SVGImageMagickAdapter.php
@@ -2,23 +2,38 @@
 
 namespace IMI\SVGProductImages;
 
+use Magento\Framework\File\Mime;
 use Magento\Framework\Image\Adapter\ImageMagick;
 
 class SVGImageMagickAdapter extends ImageMagick
 {
-	/**
-	 * Add basic SVG validation
-	 *
-	 * @param string $filePath
-	 *
-	 * @return bool
-	 */
-	public function validateUploadFile( $filePath ) {
-        $type = mime_content_type($filePath);
-        if ($type === 'image/svg+xml' || $type === 'image/svg') {
-			return true;
-		}
-		return parent::validateUploadFile( $filePath );
-	}
+    /**
+     * @var Mime
+     */
+    private $mime;
 
+    public function __construct(
+        \Magento\Framework\Filesystem $filesystem,
+        \Psr\Log\LoggerInterface $logger, array $data = [],
+        Mime $mime
+    )
+    {
+        parent::__construct($filesystem, $logger, $data);
+        $this->mime = $mime;
+    }
+
+    /**
+     * Add basic SVG validation
+     *
+     * @param string $filePath
+     *
+     * @return bool
+     */
+    public function validateUploadFile($filePath)
+    {
+        if ($this->mime->getMimeType($filePath) === 'svg') {
+            return true;
+        }
+        return parent::validateUploadFile($filePath);
+    }
 }
